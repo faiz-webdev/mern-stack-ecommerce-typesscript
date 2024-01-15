95a235a90e8f6cce05f39cdf7d3449a6797b9536
faizttn <faijuddin@tothenew.com>
2024-01-15 15:02:56 +0530

    home page design

--- ecommerce-frontend/src/pages/Home.tsx
+++ ecommerce-frontend/src/pages/Home.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='home'>
+            <section></section>
+
+
+            <h1>Latest Product
+
+                <Link to="/search" className='findmore'>More</Link>
+            </h1>
+
+            <main></main>
+        </div>
+    )
 }
 
 export default Home
