c283035169d58352e09499874c1d02efa0e60302
faizttn <faijuddin@tothenew.com>
2024-01-15 15:29:45 +0530

    product cart design

---
+++ ecommerce-frontend/src/components/ProductCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa';
+
+type ProductsProps = {
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  handler: () => void;
+}
+
+const server = "dsdsd";
+
+function ProductCard({ productId, photo, name, price, stock, handler }: ProductsProps) {
+  return (
+    <div className="product-card">
+      <img src={`${photo}`} alt={name} />
+
+      <p>{name}</p>
+      <span>₹{price}</span>
+
+      <div>
+        <button onClick={() => handler()}><FaPlus /></button>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
--- ecommerce-frontend/src/pages/Home.tsx
+++ ecommerce-frontend/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
 
 function Home() {
+    const addToCartHandler = () => { }
+
     return (
         <div className='home'>
             <section></section>
@@ -12,7 +15,11 @@ function Home() {
                 <Link to="/search" className='findmore'>More</Link>
             </h1>
 
-            <main></main>
+            <main>
+                <ProductCard productId='3323' name='watch'
+                    photo='https://m.media-amazon.com/images/I/81F9S6OzE8L._SX569_.jpg' price={100} stock={10}
+                    handler={addToCartHandler} />
+            </main>
         </div>
     )
 }
