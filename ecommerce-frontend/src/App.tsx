0a2be4df51958e0b4178ffd28d78753f0368011c
faizttn <faijuddin@tothenew.com>
2024-01-15 12:51:24 +0530

    header design

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { lazy, Suspense } from "react"
 import Loader from "./components/Loader"
+import Header from "./components/Header"
 
 const Home = lazy(() => import("./pages/Home"))
 const Search = lazy(() => import("./pages/Search"))
@@ -32,6 +33,7 @@ function App() {
   return (
     <Router>
       {/* Header */}
+      <Header />
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -40,9 +42,9 @@ function App() {
 
           {/* admin routes */}
           <Route
-            // element={
-            //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
-            // }
+          // element={
+          //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
+          // }
           >
             <Route path="/admin/dashboard" element={<Dashboard />} />
             <Route path="/admin/product" element={<Products />} />
---
+++ ecommerce-frontend/src/components/Header.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const user = {
+    _id: "sds",
+    role: "admin",
+}
+
+function Header() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const logoutHandler = () => {
+        console.log('logoutHandler')
+        setIsOpen(false);
+    }
+
+    return (
+        <nav className='header'>
+            <Link to={'/'} onClick={() => setIsOpen(false)}>
+                Home
+            </Link>
+
+            <Link to={'/search'} onClick={() => setIsOpen(false)}>
+                <FaSearch />{" "}
+            </Link>
+
+            <Link to={'/cart'} onClick={() => setIsOpen(false)}>
+                <FaShoppingBag />
+            </Link>
+
+
+            {user?._id ? (
+                <>
+                    <button onClick={() => setIsOpen((prev) => !prev)}>
+                        <FaUser />
+                    </button>
+                    <dialog open={isOpen}>
+                        <div>
+                            {user.role === 'admin' && (<Link onClick={() => setIsOpen(false)}
+                                to={'admin/dashboard'}>Admin</Link>)}
+
+                            <Link onClick={() => setIsOpen(false)} to={'/orders'}>
+                                Orders
+                            </Link>
+                            <button onClick={logoutHandler}>
+                                <FaSignOutAlt />
+                            </button>
+                        </div>
+                    </dialog>
+                </>
+            ) : (<Link to={'/login'}>
+                <FaSignInAlt />
+            </Link>)}
+        </nav>
+    )
+}
+
+export default Header
