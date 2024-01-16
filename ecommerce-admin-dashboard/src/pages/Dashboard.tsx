6d20e164651f724b35ce13f638c82979d99ac334
faizttn <faijuddin@tothenew.com>
2024-01-16 19:11:56 +0530

    design admin dashboard search section

--- ecommerce-admin-dashboard/src/pages/Dashboard.tsx
+++ ecommerce-admin-dashboard/src/pages/Dashboard.tsx
@@ -1,10 +1,22 @@
+import { BsSearch } from 'react-icons/bs'
 import AdminSidebar from '../components/AdminSidebar'
+import { FaRegBell } from 'react-icons/fa'
+
+
+const userImg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJxA5cTf-5dh5Eusm0puHbvAhOrCRPtckzjA&usqp"
 
 const Dashboard = () => {
     return (
         <div className='adminContainer'>
             <AdminSidebar />
-            <main>Main</main>
+            <main className='dashboard'>
+                <div className="bar">
+                    <BsSearch />
+                    <input type="text" placeholder='search for data, users, docs' />
+                    <FaRegBell />
+                    <img src={userImg} alt="" />
+                </div>
+            </main>
 
         </div>
     )
