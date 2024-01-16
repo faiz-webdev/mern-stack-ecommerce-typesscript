5007bb26cf3c58495ef25a45c7f9117cd49488b6
faizttn <faijuddin@tothenew.com>
2024-01-16 18:57:12 +0530

    admin leftside menu added chart and toss menu

--- ecommerce-admin-dashboard/src/components/AdminSidebar.tsx
+++ ecommerce-admin-dashboard/src/components/AdminSidebar.tsx
@@ -1,8 +1,9 @@
-import { RiDashboardFill, RiShoppingBag3Fill, } from "react-icons/ri"
+import { RiDashboardFill, RiShoppingBag3Fill, RiCoupon3Fill } from "react-icons/ri"
 import { Link, Location, useLocation } from "react-router-dom"
 import { AiFillFileText } from "react-icons/ai"
 import { IoIosPeople } from "react-icons/io"
 import { IconType } from "react-icons"
+import { FaChartBar, FaChartLine, FaChartPie, FaStopwatch, FaGamepad } from "react-icons/fa"
 
 const AdminSidebar = () => {
     const location = useLocation()
@@ -11,25 +12,64 @@ const AdminSidebar = () => {
     return (
         <aside>
             <h2>Logo.</h2>
-            <div>
-                <h5>Dashboard</h5>
-                <ul>
-                    <Li url="/admin/dashboard" text="Dashboard"
-                        Icon={RiDashboardFill} location={location} />
+            <DivOne location={location} />
+            <DivTwo location={location} />
+            <DivThree location={location} />
 
-                    <Li url="/admin/product" text="Product"
-                        Icon={RiShoppingBag3Fill} location={location} />
-
-                    <Li url="/admin/customer" text="Customer"
-                        Icon={IoIosPeople} location={location} />
-
-                    <Li url="/admin/transaction" text="Transaction"
-                        Icon={AiFillFileText} location={location} />
-                </ul>
-            </div>
         </aside>
     )
 }
+
+const DivOne = ({ location }: { location: Location }) => (
+    <div>
+        <h5>Dashboard</h5>
+        <ul>
+            <Li url="/admin/dashboard" text="Dashboard"
+                Icon={RiDashboardFill} location={location} />
+
+            <Li url="/admin/product" text="Product"
+                Icon={RiShoppingBag3Fill} location={location} />
+
+            <Li url="/admin/customer" text="Customer"
+                Icon={IoIosPeople} location={location} />
+
+            <Li url="/admin/transaction" text="Transaction"
+                Icon={AiFillFileText} location={location} />
+        </ul>
+    </div>
+)
+
+const DivTwo = ({ location }: { location: Location }) => (
+    <div>
+        <h5>Charts</h5>
+        <ul>
+            <Li url="/admin/chart/bar" text="Bar"
+                Icon={FaChartBar} location={location} />
+
+            <Li url="/admin/chart/pie" text="Pie"
+                Icon={FaChartPie} location={location} />
+
+            <Li url="/admin/chart/line" text="Line"
+                Icon={FaChartLine} location={location} />
+        </ul>
+    </div>
+)
+
+const DivThree = ({ location }: { location: Location }) => (
+    <div>
+        <h5>Apps</h5>
+        <ul>
+            <Li url="/admin/app/stopwatch" text="Stopwatch"
+                Icon={FaStopwatch} location={location} />
+
+            <Li url="/admin/app/coupon" text="Coupon"
+                Icon={RiCoupon3Fill} location={location} />
+
+            <Li url="/admin/app/toss" text="Toss"
+                Icon={FaGamepad} location={location} />
+        </ul>
+    </div>
+)
 
 interface LiProps {
     url: string,
