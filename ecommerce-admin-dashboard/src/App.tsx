f4559ad9750a0c3bb6e9f1b7cb48fabd4d5f6650
faizttn <faijuddin@tothenew.com>
2024-01-16 16:06:43 +0530

    implement route

--- ecommerce-admin-dashboard/src/App.tsx
+++ ecommerce-admin-dashboard/src/App.tsx
@@ -1,7 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Suspense, lazy } from "react"
+import Loader from "./components/Loader"
+
+
+
+const Dashboard = lazy(() => import("./pages/Dashboard"))
+const Products = lazy(() => import("./pages/Products"))
+const Customer = lazy(() => import("./pages/Customer"))
+const Transactions = lazy(() => import("./pages/Transactions"))
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/customer" element={<Customer />} />
+          <Route path="/admin/transaction" element={<Transactions />} />
+
+          {/* Charts */}
+
+          {/* Apps */}
+
+        </Routes>
+      </Suspense>
+    </Router>
   )
 }
 
